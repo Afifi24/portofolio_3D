ad3a76c574359f45a80d7d49260d430e1452237f
afifi brahim <afifi71brahim@gmail.com>
2023-04-09 01:13:48 +0000

    add some functionnality

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-
+import { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls,Preload, useGLTF } from '@react-three/drei'
+import CanvasLoader from '../Loader'
 const Earth = () => {
   return (
     <div>Earth</div>
   )
 }
+
 
 export default Earth
